0d4ef5673d5e56ebc56f09748d060e96c7122095
Higor Rocha <you@example.com>
2021-08-17 10:08:06 -0300

    Creating project.

---
+++ android/app/src/main/kotlin/me/higorrocha/youtube_clone_bt/MainActivity.kt
@@ -0,0 +1,6 @@
+package me.higorrocha.youtube_clone_bt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_clone_bt/Home.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
